9d007d1da0d676c2212724cccf5d49a5640491ce
heartistizio <tiziopl@gmail.com>
2019-04-24 18:23:12 +0200

    Added 2nd versoin of Luck Balance

--- src/main/scala/InterviewPreparationKit/GreedyAlgorithms/LuckBalance.scala
+++ src/main/scala/InterviewPreparationKit/GreedyAlgorithms/LuckBalance.scala
@@ -9,11 +9,33 @@ class LuckBalance {
     val importantContests: mutable.PriorityQueue[Int] = new mutable.PriorityQueue[Int]
     contests.foreach(e => {
       if(e(1) == 0) luckBalance += e(0)
-      else importantContests += e(0)
+      else {
+        importantContests += e(0)
+        if(importantContests.length > k) luckBalance -= importantContests.dequeue()
+      }
     })
-    for(i <- 1 to importantContests.length){
-      if(i <= k) luckBalance += importantContests.dequeue
-      else luckBalance -= importantContests.dequeue
+    for(i <- 0 until k){
+      luckBalance += importantContests.dequeue
+    }
+    luckBalance
+  }
+
+  def luckBalance2(k: Int, contests: Array[Array[Int]]): Int = {
+    var luckBalance: Int = 0
+    var importantContests: mutable.PriorityQueue[Int] = new mutable.PriorityQueue[Int]
+    contests.foreach(e => {
+      if(e(1) == 0) luckBalance += e(0)
+      else {
+        importantContests += e(0)
+        if(importantContests.length > k) {val reverseIC = importantContests.reverse
+          luckBalance -= reverseIC.dequeue
+          importantContests = reverseIC.reverse
+        }
+      }
+    })
+    println(importantContests)
+    for(i <- 0 until k){
+      luckBalance += importantContests.dequeue
     }
     luckBalance
   }
